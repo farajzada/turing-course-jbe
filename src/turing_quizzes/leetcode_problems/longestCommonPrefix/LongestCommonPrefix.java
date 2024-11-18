d65776744f6e9674221d26e190d1d1a867e6ac45
muradfarajzada <mfarajzadaa@gmail.com>
2024-11-18 17:00:47 +0400

    LongestCommonPrefix.java  task attached

--- src/turing_quizzes/leetcode_problems/longestCommonPrefix/LongestCommonPrefix.java
+++ src/turing_quizzes/leetcode_problems/longestCommonPrefix/LongestCommonPrefix.java
@@ -11,7 +11,6 @@ public class LongestCommonPrefix {
         for (int i = 1; i < strs.length; i++) { // check  prf other strings
             while (strs[i].indexOf(prefix) != 0) { // check prf match main string
                 prefix = prefix.substring(0, prefix.length() - 1);
-//                if (prefix.isEmpty()) return "";
 
             }
         }
@@ -26,16 +25,4 @@ public class LongestCommonPrefix {
         System.out.println(new LongestCommonPrefix().longestCommonPrefix(a3));
 
     }
-
-
-    //    public String longestCommonPrefix(String[] strs) {
-//        if (strs == null || strs.length == 0) return "";
-//        for (int i = 0; i < strs[0].length(); i++) {
-//            char c = strs[0].charAt(i);
-//            for (int j = 1; j < strs.length; j++) {
-//                if(i>=strs[j].length() || strs[j].charAt(i) != c) return strs[0].substring(0, i);
-//            }
-//        }
-//        return strs[0];
-//    }
 }
